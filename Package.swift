f4081dbd1a1793217686a381330e90b6626710c7
marinofaggiana <marino@marinofaggiana.com>
2021-10-01 11:07:22 +0200

    change to 12

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "Parchment",
-    platforms: [.iOS("9.0")],
+    platforms: [.iOS("12.0")],
     products: [
         .library(name: "Parchment", targets: ["Parchment"]),
     ],
